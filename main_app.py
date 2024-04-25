165af70f6f1b0582e67d7da3b48ec97192323797
Piotr-Omiecina <263431@student.pwr.edu.pl>
2024-04-25 22:13:49 +0200

    added GUI

---
+++ main_app.py
@@ -0,0 +1,27 @@
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QMouseEvent
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5 import uic
+from endpoints import *
+
+class MyWidget(QWidget):
+    def __init__(self):
+        super(MyWidget, self).__init__()
+        uic.loadUi("form.ui", self)
+        self.show()
+        self.Quit.clicked.connect(lambda: quit())
+        self.Connect.clicked.connect(lambda: )
+
+
+def connect():
+    message = QMessageBox()
+    message.setText("Connected!")
+    message.exec()
+def main():
+    app  = QApplication([])
+    widnow  = MyWidget()
+    app.exec()
+
+if __name__== '__main__':
+    main()
