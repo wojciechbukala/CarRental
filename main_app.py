6d795a585959b0fd3a7dd3057ad6b9c59ad546df
Piotr-Omiecina <263431@student.pwr.edu.pl>
2024-05-25 13:06:51 +0200

    fixed searchBar, added hiding menus and some visual teaks

--- main_app.py
+++ main_app.py
@@ -28,8 +28,10 @@ class Customer_Dashboard(QMainWindow):
         super().__init__(parent)
         self.ui = Ui_MainWindow_customer()
         self.ui.setupUi(self)
+        self.ui.leftMenu.hide()
+        self.ui.profileMenu.hide()
         self.customer_data = get_customer_by_email(email)[1][0]
-        self.ui.customerName.setText(self.customer_data[1])
+        self.ui.customer_name.setText(self.customer_data[1])
         self.availableCars = get_available_cars()
         self.fillCarsTable()
         # print("Response:")
@@ -37,48 +39,82 @@ class Customer_Dashboard(QMainWindow):
     def fillCarsTable(self):
         id=1
         for car in self.availableCars:
-            self.tmpBrand = QLabel(self.ui.frame_6)
-            self.tmpBrand.setText(car[1])
-            self.ui.gridLayout.addWidget(self.tmpBrand, id, 0, 1, 1)
-            self.tmpModel = QLabel(self.ui.frame_6)
-            self.tmpModel.setText(car[0])
-            self.ui.gridLayout.addWidget(self.tmpModel, id, 1, 1, 1)
-            self.tmpYear = QLabel(self.ui.frame_6)
-            self.tmpYear.setText(car[2])
-            self.ui.gridLayout.addWidget(self.tmpYear, id, 2, 1, 1)
-            self.tmpPrice = QLabel(self.ui.frame_6)
-            self.tmpPrice.setText("$100")
-            self.ui.gridLayout.addWidget(self.tmpPrice, id, 3, 1, 1)
-            self.tmpBtn = rentCarBtn(car[1],car[0])
-            self.tmpBtn.setText("Rent now!")
-            sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Preferred)
-            sizePolicy.setHorizontalStretch(0)
-            sizePolicy.setVerticalStretch(0)
-            sizePolicy.setHeightForWidth(self.tmpBtn.sizePolicy().hasHeightForWidth())
-            self.tmpBtn.setSizePolicy(sizePolicy)
-            self.tmpBtn.setMinimumSize(QtCore.QSize(100, 30))
-            self.tmpBtn.setStyleSheet("*{\n"
+            self.createNewRentOffer(car, id)
+            if id==5: break
+            id+=1
+
+    def createNewRentOffer(self, car, row):
+        self.addBrand(car, row)
+        self.addModel(car, row)
+        self.addYear(car, row)
+        self.addPrice(car, row)
+        self.addRentBtn(car, row)
+
+    def addBrand(self, car, row):
+        self.tmpBrand = QLabel(self.ui.frame_6)
+        self.tmpBrand.setText(car[1])
+        self.ui.gridLayout.addWidget(self.tmpBrand, row, 0, 1, 1)
+
+    def addModel(self, car, row):
+        self.tmpModel = QLabel(self.ui.frame_6)
+        self.tmpModel.setText(car[0])
+        self.ui.gridLayout.addWidget(self.tmpModel, row, 1, 1, 1)
+
+    def addYear(self, car, row):
+        self.tmpYear = QLabel(self.ui.frame_6)
+        year = car[2].split(" ")[3]
+        self.tmpYear.setText(year)
+        self.ui.gridLayout.addWidget(self.tmpYear, row, 2, 1, 1)
+
+    def addPrice(self, car, row):
+        self.tmpPrice = QLabel(self.ui.frame_6)
+        self.tmpPrice.setText("$100")
+        self.ui.gridLayout.addWidget(self.tmpPrice, row, 3, 1, 1)
+
+    def addRentBtn(self, car, row):
+        self.tmpBtn = rentCarBtn(car[1],car[0])
+        self.tmpBtn.setText("Rent now!")
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Preferred)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.tmpBtn.sizePolicy().hasHeightForWidth())
+        self.tmpBtn.setSizePolicy(sizePolicy)
+        self.tmpBtn.setMinimumSize(QtCore.QSize(100, 30))
+        self.tmpBtn.setCursor(QtGui.QCursor(QtCore.Qt.PointingHandCursor))
+        self.tmpBtn.setStyleSheet("*{\n"
                                     "    background-color: #2596be;\n"
                                     "    border-radius: 10px;\n"
                                     "}\n")
-            self.ui.gridLayout.addWidget(self.tmpBtn, id, 4, 1, 1)
-            if id==5: break
-            id+=1
-        pass
+        self.ui.gridLayout.addWidget(self.tmpBtn, row, 4, 1, 1)
+        
     def on_startDate_select(self, date):    
         self.ui.startDateLabel.setText(date.toString("dd.MM.yyyy"))
     def on_endDate_select(self, date):
         self.ui.endDateLabel.setText(date.toString("dd.MM.yyyy"))
+
     def on_startDateBtn_pressed(self):
         self.calendar = QCalendarWidget()
         self.calendar.clicked.connect(self.on_startDate_select)
         self.calendar.clicked.connect(self.calendar.close)
         self.calendar.show()
+
     def on_endDateBtn_pressed(self):
         self.calendar = QCalendarWidget()
         self.calendar.clicked.connect(self.on_endDate_select)
         self.calendar.clicked.connect(self.calendar.close)
         self.calendar.show()
+
+    def on_menuBtn_pressed(self):
+        if(self.ui.leftMenu.isVisible()):
+            self.ui.leftMenu.hide()
+        else:
+            self.ui.leftMenu.show()
+    def on_accountBtn_pressed(self):
+        if(self.ui.profileMenu.isVisible()):
+            self.ui.profileMenu.hide()
+        else:
+            self.ui.profileMenu.show()
+
 class LoginRegister(QWidget):
     def __init__(self, parent=None):
         super().__init__(parent)
